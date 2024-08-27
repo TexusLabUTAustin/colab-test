c7e0b3d313e1299986caccb3ca1953ab7cb2916e
Adi <adi@wireless-10-147-80-115.public.utexas.edu>
2024-08-27 11:37:01 -0500

    Phone versoin + links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,57 +1,33 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const navigate = useNavigate();
 
     const toggleNavbar = () => {
         setNavbarOpen(!navbarOpen);
     };
 
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
+    const handleNavigation = (path) => {
+        navigate(path);
+        setNavbarOpen(false);
     };
 
     return (
         <nav className="navbar">
             <div className="navbar-header">
-            
                 <button className="navbar-toggle" onClick={toggleNavbar}>
                     ☰
                 </button>
             </div>
-            <ul className={`navbar-links ${navbarOpen ? 'show' : ''}`}>
-                <li className="nav-item">
-                    <Link to="/" onClick={toggleNavbar}>Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/about" onClick={toggleNavbar}>About</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/projects" onClick={toggleNavbar}>Projects</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/contact" onClick={toggleNavbar}>Contact</Link>
-                </li>
-                {/*
-                <li className="nav-item dropdown" onClick={toggleDropdown}>
-                    More
-                    <ul className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}>
-                        <li className="dropdown-item">
-                            <Link to="/action1" onClick={toggleNavbar}>Action 1</Link>
-                        </li>
-                        <li className="dropdown-item">
-                            <Link to="/action2" onClick={toggleNavbar}>Action 2</Link>
-                        </li>
-                        <li className="dropdown-item">
-                            <Link to="/action3" onClick={toggleNavbar}>Action 3</Link>
-                        </li>
-                    </ul>
-                </li>
-                */}
-            </ul>
+            <div className={`navbar-links ${navbarOpen ? 'show' : ''}`}>
+                <button className="nav-button" onClick={() => handleNavigation('/')}>Home</button>
+                <button className="nav-button" onClick={() => handleNavigation('/about')}>About</button>
+                <button className="nav-button" onClick={() => handleNavigation('/projects')}>Projects</button>
+                <button className="nav-button" onClick={() => handleNavigation('/contact')}>Contact</button>
+            </div>
         </nav>
     );
 };
