ee7d47ae931428b7124393a9423aadb1512ec103
pgrover <parikagrover@gmail.com>
2024-10-01 13:05:04 -0500

    project page old UI

--- src/Components/ProjectCardContent.js
+++ src/Components/ProjectCardContent.js
@@ -17,21 +17,23 @@ function ProjectCardContent({ title, pictureSrc, content, reportName, buttonText
   return (
 
     <div className="cards">
-    {/* <Link to={`/content/${targetSection}`} className="card-link"> */}
-    <div>
-      <img src={process.env.PUBLIC_URL + "/images/" + pictureSrc} alt={title} className="card-picture" />
-    </div>
-    <h2 className="card-title">{title}</h2>
-    <p className="card-content">{content}</p>
-    <div class="btn">
+      {/* <Link to={`/content/${targetSection}`} className="card-link"> */}
+      <div class="image-container">
+      <img src={process.env.PUBLIC_URL+ "/images/" +  pictureSrc} alt={title} className="card-picture" />
+      </div>
+      <div class="card-content">
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <div class="btn">
       <a href={`${process.env.PUBLIC_URL}/pdf/${reportName}`} className="pdf-button" target="_blank" rel="noopener noreferrer">
-        <div className="pdf-button-content">View Full Report</div>
-      </a>
+            <div className="pdf-button-content">View Full Report</div>
+          </a>
+      </div>
+      </div>
+      
+          {/* </Link> */}
+       {/*<ScrollButton buttonText={buttonText || "Know More"} targetSection={targetSection} />*/}
     </div>
-    {/* </Link> */}
-    {/*<ScrollButton buttonText={buttonText || "Know More"} targetSection={targetSection} />*/}
-  </div>
-
   );
 }
 
